a072747bfe89bfababd2619427d3eb42ac45a3c9
AxXxel001 <alexander.schaefer@uni-duesseldorf.de>
2013-06-01 10:43:29 +0200

    BattleSystem erweitert

--- src/Scene_BattleSystem.java
+++ src/Scene_BattleSystem.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 
 
 enum BattleType {
+	NONE,
 	ANIMATION,
-	WAIT_FOR_PLAYER
+	WAIT_FOR_PLAYER,
+	WAIT_FOR_ENEMY
 }
 
 public class Scene_BattleSystem extends Abstract_Scene {
@@ -13,12 +15,25 @@ public class Scene_BattleSystem extends Abstract_Scene {
 	private ArrayList<Object_BattleActor> action_order;
 	private BattleType battle_type;
 	
+	private Object_BattleActor current_actor;
+	private SubScene_WindowSelectable menu;
+	
 	Scene_BattleSystem(Object_BattleContext ctx, Abstract_Scene previous_scene, Object_Game game) {
 		super(game);
 		this.ctx = ctx;
 		this.previous_scene = previous_scene;
 		this.action_order = new ArrayList<Object_BattleActor>();
-		this.battle_type = BattleType.WAIT_FOR_PLAYER;
+		this.battle_type = BattleType.NONE;
+		
+		this.current_actor = null;
+		this.menu = new SubScene_WindowSelectable(0, 0, game);
+		this.menu.EXIT_POSSIBLE = false;
+		this.menu.addCommand("Angriff");
+		this.menu.addCommand("Skills");
+		this.menu.addCommand("Item");
+		this.menu.addCommand("Flucht");
+		
+		this.sortActors();
 	}
 
 	@Override
@@ -36,8 +51,36 @@ public class Scene_BattleSystem extends Abstract_Scene {
 
 	@Override
 	public void updateData() {
-		// TODO Auto-generated method stub
-		
+		if (this.current_actor == null) {
+			this.current_actor = this.action_order.get(0);
+			if (this.ctx.getPlayers().contains(this.current_actor)) {
+				this.battle_type = BattleType.WAIT_FOR_PLAYER;
+			}
+			else {
+				this.battle_type = BattleType.WAIT_FOR_ENEMY;
+			}
+		}
+		if (this.battle_type == BattleType.WAIT_FOR_PLAYER) {
+			if (this.menu.EXECUTED) {
+				this.menu.updateData();
+			}
+			else {
+				switch (this.menu.cursor) {
+				case 0: //Angriff
+					break;
+				case 1: //Skills
+					break;
+				case 2: //Item
+					break;
+				case 3: //Flucht
+					break;
+				}
+			}
+			this.current_actor = null;
+		}
+		else if (this.battle_type == BattleType.WAIT_FOR_ENEMY) {
+			this.current_actor = null;
+		}
 	}
 
 	@Override
@@ -47,12 +90,8 @@ public class Scene_BattleSystem extends Abstract_Scene {
 	}
 	
 	private void sortActors() {
-		this.action_order.clear();
-		while (this.action_order.size() < 10) {
-			java.util.Collections.sort(this.ctx.getActors());
-			this.action_order.add(this.ctx.getActors().get(0));
-			this.ctx.getActors().get(0).reduceSpeed();
-		}
+		java.util.Collections.sort(this.ctx.getActors());
+		this.action_order = this.ctx.getActors();
 	}
 
 }
