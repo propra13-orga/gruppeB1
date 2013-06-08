e5df4bcf0ba836bc0040731e39ad62c5645ed3f3
o-o-x-o <victor.persien@hhu.de>
2013-06-08 21:15:03 +0200

    modified:   src/Object_EntityData.java 	new file:   src/Object_RoomData.java 	deleted:    src/Component_Health.java

--- src/Object_EntityData.java
+++ src/Object_EntityData.java
@@ -1,6 +1,5 @@
 import java.util.Hashtable;
 
-
 public class Object_EntityData extends Hashtable<String,String> {
 
 	private static final long serialVersionUID = -7494399952808211043L;
---
+++ src/Object_RoomData.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Object_RoomData extends ArrayList<Object_RoomData> {
+
+	private static final long serialVersionUID = -8601054676257239894L;
+
+}
