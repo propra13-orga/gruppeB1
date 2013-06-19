22af09fba172a83f2d66068f9d831f4168206641
Hyojin Lee <hyojin.lee@uni-duesseldorf.de>
2013-06-20 00:04:50 +0200

    zusammenhang von aller Info von jeder Zimmer
    er erfasst 2 Teile
    1. int[][][] von map
    2. LinkedHashMap Entity_List[] von aller Object in dieser Room

--- src/Object_RoomData.java
+++ src/Object_RoomData.java
@@ -1,18 +1,22 @@
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Object_RoomData {
 
 	int[][][] room;
-	List<Object_EntityData> entityData;
+	List<LinkedHashMap> entityData;
 	
 	public Object_RoomData() {
-		this.entityData = new LinkedList<Object_EntityData>();
+		this.entityData = new LinkedList<LinkedHashMap>();
 	}
 	
+	//setRoom und addEntityData dienen dazu, 
+	//dass alle num[][][] von Tmx. und Entity_List[] ins room[][][] und entityData abzuspeichern.
 	public void setRoom(int[][][] room) { this.room = room; }
-	public void addEntityData(Object_EntityData data) { this.entityData.add(data); }
+	public void addEntityData(LinkedHashMap data) { this.entityData.add(data); }
 	
+	//umkehrte Funktion
 	public int[][][] getRoom() { return this.room; }
-	public List<Object_EntityData> getEntityData() { return this.entityData; }
+	public List<LinkedHashMap> getEntityData() { return this.entityData; }
 }
