40c7486243991230a4ba97b8470d4d873dc5faa4
Elina Magamaeva <Elina.Magamaeva@uni-duesseldorf.de>
2013-07-11 01:16:19 +0200

    Einige Kommenatare hinzugefuegt

--- src/Scene_GameOver.java
+++ src/Scene_GameOver.java
@@ -10,7 +10,7 @@ public class Scene_GameOver extends Abstract_Scene {
 	Window_Menu menu;
 	BufferedImage background;
 
-	Scene_GameOver(Object_Game game) {
+	Scene_GameOver(Object_Game game) { // Scene_GameOver erbt die Eigentschafte von Object_Game
 		super(game);
 		
 		String path = "res/pictures/gameover.png";
@@ -27,37 +27,37 @@ public class Scene_GameOver extends Abstract_Scene {
 		}
 
 	
-	menu=new Window_Menu(game, "Auswahl");
-	menu.addReturnCommand("Spiel neu starten");
-	menu.addReturnCommand("Zurueck ins Hauptmenue");
-	menu.addReturnCommand("Spiel beenden");
-	menu.center();
-	menu.y += 120;
+	menu=new Window_Menu(game, "Auswahl"); // ein neues Objekt der Klasse Window_Menu
+	menu.addReturnCommand("Spiel neu starten");     // Button "Spiel neu starten"
+	menu.addReturnCommand("Zurueck ins Hauptmenu"); // Button "Zurueck ins Hauptmenu"
+	menu.addReturnCommand("Spiel beenden"); // Button "Spiel beenden"
+	menu.center(); // die Position (X-Achse) des Menufensters
+	menu.y += 100; // die Position (Y-Achse) des Menufensters 
 	Window_Menu.setMainMenu(menu);
 	
 	}
 
 	@Override
 	public void onStart() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void onExit() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void updateData() {
-		if(menu.isExecuted()){
-			menu.updateData();
+		if(menu.isExecuted()){       // Wenn das Menu ausgeführt soll das Menu immer neu aktualisiert werden,
+           menu.updateData();       // Das passiert so schnell, dass man das mit bloßem Auge nicht wahrnimmt.
 		}
-		
-		else {
+
+		else {                     // Sonst soll einer der drei folgenden Fälle ausgeführt werden.
 			menu.setupMenuPath();
-			switch(menu.getCurrentCursor()){
+			switch(menu.getCurrentCursor()){ // Je nach dem wo der Cursor sich befindet, soll in die entsperechende Scene gewechselt werden.
 			
 			case 0:
 				game.switchScene(new Scene_Level(game), true);
@@ -74,7 +74,7 @@ public class Scene_GameOver extends Abstract_Scene {
 			}
 	}
 
-	@Override
+	@Override //Abbildung des Hintergrundbildes über das ganze Fenster
 	public void updateScreen() {
     this.screen.drawImage(this.background,0,0, null);
     
